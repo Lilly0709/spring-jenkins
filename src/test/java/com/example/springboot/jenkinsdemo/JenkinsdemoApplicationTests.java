600749da1b498a58f637de86814ab8d5a089a83d
lilly0709 <fousia2705@gmail.com>
2025-08-28 17:50:51 -0700

    first commit

---
+++ src/test/java/com/example/springboot/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.example.springboot.jenkinsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsdemoApplicationTests 
+{
+	public static Logger logger = LoggerFactory.getLogger(JenkinsdemoApplicationTests.class);
+	@Test
+	public void contextLoads() 
+	{
+		logger.info("Test case executing");
+	}
+
+}
