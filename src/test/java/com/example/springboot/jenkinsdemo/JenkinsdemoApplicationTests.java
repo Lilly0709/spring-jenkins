d1989f190621e7f831e5fbd462ec016a57b2b68f
lilly0709 <fousia2705@gmail.com>
2025-09-02 16:57:04 -0700

    Testing pipeline with build pipeline

--- src/test/java/com/example/springboot/jenkinsdemo/JenkinsdemoApplicationTests.java
+++ src/test/java/com/example/springboot/jenkinsdemo/JenkinsdemoApplicationTests.java
@@ -10,10 +10,8 @@ class JenkinsdemoApplicationTests
 {
 	public static Logger logger = LoggerFactory.getLogger(JenkinsdemoApplicationTests.class);
 	@Test
-	public  contextLoads()
+	public void contextLoads()
 	{
-		logger.info("Test case executing");
-		logger.info("Test case executing second log");
 		logger.info("Testing pipeline");
 	}
 
